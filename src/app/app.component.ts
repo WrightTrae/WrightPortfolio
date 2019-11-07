16339afeb7ac2022ae9bab7aede19a908d8fc921
WrightTrae <trae.wright12@gmail.com>
2019-11-07 08:20:00 -0500

    Add grid layout; hide/show toolbar on scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+selector: 'app-root',
+templateUrl: './app.component.html',
+styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'WrightPortfolio';
+
+export class AppComponent implements OnInit {
+	title = 'WrightPortfolio';
+
+	ngOnInit() {
+		window.onscroll = function() {
+			var currentScrollPos = window.pageYOffset;
+			if (this.prevScrollpos > currentScrollPos) {
+				document.getElementById("toolbar").style.top = "0";
+			} else {
+				document.getElementById("toolbar").style.top = "-50px";
+			}
+			this.prevScrollpos = currentScrollPos;
+		}.bind(this);
+	}
 }
